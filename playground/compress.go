a056a5a780c19062804b585f354f57bddc607121
huanglifeng <hfeeki@outlook.com>
2018-05-15 20:32:22 +0800

    signtx broadcast working

---
+++ playground/compress.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"bytes"
+	"compress/gzip"
+	"fmt"
+	"encoding/base64"
+	"io/ioutil"
+)
+
+const (
+	rawData = "01000000018f69e09027dc2c02b16bfa51e6670334d34678b7ae31a21bab01ed81258ff53e000000006441ee57686955886b9a6d90f9d561164aa8986d8e41c45fd175f0b4313784ade2f03f038e37a14e64de6682f9013875cfc6427ac1477c50208083a9d95002f3d7b101210259c2bd7f9d7d0a8c0b00a1a1124d513f214898638782dfe064b18bd8d7f0bb8cffffffff0130abdf03000000001976a91482e81438d7fa15ce205a9683dc786c241bc820f288ac00000000"
+)
+
+func main() {
+	var b bytes.Buffer
+	gz := gzip.NewWriter(&b)
+	if _, err := gz.Write([]byte(rawData)); err != nil {
+		panic(err)
+	}
+	if err := gz.Flush(); err != nil {
+		panic(err)
+	}
+	if err := gz.Close(); err != nil {
+		panic(err)
+	}
+	fmt.Printf("raw len: %d\n", len(rawData))
+	str := base64.StdEncoding.EncodeToString(b.Bytes())
+	fmt.Printf("len: %d, %s\n", len(str), str)
+	data, _ := base64.StdEncoding.DecodeString(str)
+	fmt.Printf("len: %d, %v\n", len(data), data)
+	rdata := bytes.NewReader(data)
+	r,_ := gzip.NewReader(rdata)
+	s, _ := ioutil.ReadAll(r)
+	fmt.Printf("len: %d, %v\n", len(s), string(s))
+
+}
---
+++ playground/compress2.go
@@ -0,0 +1,131 @@
+package main
+
+import (
+	"bytes"
+	"compress/flate"
+	"compress/gzip"
+	"compress/zlib"
+	"errors"
+	"fmt"
+	"io/ioutil"
+	"math/rand"
+	"time"
+
+	"github.com/gogo/protobuf/proto"
+	//"github.com/zvelo/msg"
+
+	pb "conseweb.com/wallet/icebox/protos"
+)
+
+var letters = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
+
+func randSeq(n int) string {
+	b := make([]rune, n)
+	for i := range b {
+		b[i] = letters[rand.Intn(len(letters))]
+	}
+	return string(b)
+}
+
+func check(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
+func main() {
+	var (
+		buf     bytes.Buffer
+		n       int
+		s       time.Time
+		elapsed time.Duration
+		err     error
+		obj     []byte
+
+		//test =  msg.Request{
+		//	Id: randSeq(1024 * 2),
+		//}
+	)
+
+	xmsg := randSeq(1024 )
+	test := pb.Error {
+		Code: pb.NewInt32(1),
+		Message: &xmsg,
+	}
+
+	t, err := proto.Marshal(&test)
+	check(err)
+	fmt.Printf("size of protobuf: %d\n\n", len(t))
+
+	buf.Reset()
+	fw, err := flate.NewWriter(&buf, 9)
+	check(err)
+	defer fw.Close()
+
+	s = time.Now()
+	n, err = fw.Write(t)
+	check(err)
+	fw.Close() // need to explicitly close and not just flush to prevent EOF error
+	elapsed = time.Since(s)
+
+	obj = buf.Bytes()
+	fmt.Printf("written to flated: %d\n", n)
+	fmt.Printf("size of flated: %d\n", len(obj))
+	fmt.Printf("elapsed flated: %v\n\n", elapsed)
+	err = decompressFlate(obj, &test)
+	check(err)
+
+	buf.Reset()
+	zw := zlib.NewWriter(&buf)
+	defer zw.Close()
+
+	s = time.Now()
+	n, err = zw.Write(t)
+	check(err)
+	zw.Flush()
+	elapsed = time.Since(s)
+
+	obj = buf.Bytes()
+	fmt.Printf("written to zipped: %d\n", n)
+	fmt.Printf("size of zlib: %d\n", len(obj))
+	fmt.Printf("elapsed zlib: %v\n\n", elapsed)
+
+	buf.Reset()
+	gw := gzip.NewWriter(&buf)
+	defer gw.Close()
+
+	s = time.Now()
+	n, err = gw.Write(t)
+	check(err)
+	gw.Flush()
+	elapsed = time.Since(s)
+
+	obj = buf.Bytes()
+	fmt.Printf("written to gzipped: %d\n", n)
+	fmt.Printf("size of gzip: %d\n", len(obj))
+	fmt.Printf("elapsed gzip: %v\n\n", elapsed)
+
+}
+
+func decompressFlate(obj []byte, expected *pb.Error) error {
+	fmt.Println("Decompressing")
+
+	data := bytes.NewReader(obj)
+	r := flate.NewReader(data)
+
+	enflate, err := ioutil.ReadAll(r)
+	if err != nil {
+		panic(err)
+	}
+
+	result := pb.Error{}
+	proto.Unmarshal(enflate, &result)
+
+	if result.GetCode() != expected.GetCode() {
+		fmt.Printf("RESULT[%s] != EXPECTED[%s]\n", result.GetCode(), expected.GetCode())
+		return errors.New("not equal")
+	}
+
+	fmt.Println("DECOMPRESSION EQUAL")
+	return nil
+}
